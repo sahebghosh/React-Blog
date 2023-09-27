88aefcd463fa52f4e63427ec88b5750f3036b73b
Saheb Ghosh <saheb.ghosh52@gmail.com>
2023-09-27 15:35:55 +0200

    BrowserRouter, Route, Switch are added.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import AddBlog from "./components/AddBlog";
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div>
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <AddBlog />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
---
+++ src/components/AddBlog.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AddBlog() {
+  return (
+    <div>
+      <h2>Add new blog!!!</h2>
+      <p>Complete functionality is coming soon.....</p>
+    </div>
+  );
+}
+
+export default AddBlog;
